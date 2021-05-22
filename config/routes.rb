19bf421fbe22cccc93a4553164b789ab12d7fca2
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-22 05:13:51 +0000

    [Update favorite機能追加]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'homes#top'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :post_comments, only:[:create,:destroy]
+  root 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   end
